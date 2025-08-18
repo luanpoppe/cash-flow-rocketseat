b87c3bce899d3a4a6628fca74dae3512082142d3
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-18 10:58:32 -0300

    feat: adicionando primeiras validações do r egistro de uma despesa

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,3 +1,4 @@
+using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
@@ -7,6 +8,23 @@ public class RegisterExpenseUseCase
 {
   public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
   {
+    Validate(request);
+
     return new ResponseRegisterExpenseJson();
   }
+
+  private void Validate(RequestRegisterExpenseJson request)
+  {
+    var isTitleEmpty = string.IsNullOrWhiteSpace(request.Title);
+    if (isTitleEmpty) throw new ArgumentException("The title is required");
+
+    if (request.Amount <= 0) throw new ArgumentException("The amount must be greater than 0");
+
+    var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+    if (result > 0) throw new ArgumentException("Expenses cannot be for the future");
+
+    var isPaymentTypeValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+    if (!isPaymentTypeValid) throw new ArgumentException("Payment Type is not valid");
+  }
+
 }
