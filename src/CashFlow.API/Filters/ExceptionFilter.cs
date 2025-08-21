75e78060e2e1577a0dabd373c9418de9967fe8c7
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-21 15:53:06 -0300

    feat: adicionando arquivo de resource para lidar com mensagens de erro

--- src/CashFlow.API/Filters/ExceptionFilter.cs
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -1,4 +1,5 @@
 using CashFlow.Communication.Responses;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -40,7 +41,7 @@ public class ExceptionFilter : IExceptionFilter
 
   private void ThrowUnknowError(ExceptionContext context)
   {
-    var errorResponse = new ResponseErrorJson("Unknow Error");
+    var errorResponse = new ResponseErrorJson(ResourceErrorMessages.UNKNOW_ERROR);
     context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
     context.Result = new ObjectResult(errorResponse);
   }
