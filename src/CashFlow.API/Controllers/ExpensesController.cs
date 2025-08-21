68a32d4703a613a769c535444603ba859cf7e331
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-21 11:15:13 -0300

    feat: terminando de configurar o filtro de exceções

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,7 +1,5 @@
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
-using CashFlow.Communication.Responses;
-using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.API.Controllers;
@@ -13,21 +11,8 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        try
-        {
-            var useCase = new RegisterExpenseUseCase();
-            var response = useCase.Execute(request);
-            return Created(string.Empty, response);
-        }
-        catch (ErrorOnValidationException ex)
-        {
-            var errorResponse = new ResponseErrorJson(ex.Errors);
-            return BadRequest(errorResponse);
-        }
-        catch
-        {
-            var errorResponse = new ResponseErrorJson("Unknow Error");
-            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-        }
+        var useCase = new RegisterExpenseUseCase();
+        var response = useCase.Execute(request);
+        return Created(string.Empty, response);
     }
 }
