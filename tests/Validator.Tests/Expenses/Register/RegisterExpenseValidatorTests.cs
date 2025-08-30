966183845b24821eeeffaa8d0de1444782d2d6ed
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-30 11:08:31 -0300

    feat: adicionando maisa testes de unidade do validador de registros de despesa

--- tests/Validator.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validator.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,6 @@
 using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using Shouldly;
 
@@ -15,5 +17,67 @@ public class RegisterExpenseValidatorTests
     var result = validator.Validate(request);
 
     result.IsValid.ShouldBeTrue();
+  }
+
+  [Theory]
+  [InlineData("")]
+  [InlineData("          ")]
+  [InlineData(null)]
+  public void Error_Title_Empty(string title)
+  {
+    var validator = new RegisterExpenseValidator();
+    var request = RequestRegisterExpenseJsonBuilder.Build();
+    request.Title = title;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.ShouldBeFalse();
+    result.Errors.ShouldHaveSingleItem();
+    result.Errors[0].ErrorMessage.ShouldBe(ResourceErrorMessages.TITLE_REQUIRED);
+  }
+
+  [Fact]
+  public void Error_Date_Future()
+  {
+    var validator = new RegisterExpenseValidator();
+    var request = RequestRegisterExpenseJsonBuilder.Build();
+    request.Date = DateTime.UtcNow.AddDays(1);
+
+    var result = validator.Validate(request);
+
+    result.IsValid.ShouldBeFalse();
+    result.Errors.ShouldHaveSingleItem();
+    result.Errors[0].ErrorMessage.ShouldBe(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE);
+  }
+
+  [Fact]
+  public void Error_Payment_Type_Invalid()
+  {
+    var validator = new RegisterExpenseValidator();
+    var request = RequestRegisterExpenseJsonBuilder.Build();
+    request.PaymentType = (PaymentType)700;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.ShouldBeFalse();
+    result.Errors.ShouldHaveSingleItem();
+    result.Errors[0].ErrorMessage.ShouldBe(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
+  }
+
+  [Theory]
+  [InlineData(0)]
+  [InlineData(-1)]
+  [InlineData(-7)]
+  public void Error_Amount_Invalid(decimal amount)
+  {
+    var validator = new RegisterExpenseValidator();
+    var request = RequestRegisterExpenseJsonBuilder.Build();
+    request.Amount = amount;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.ShouldBeFalse();
+    result.Errors.ShouldHaveSingleItem();
+    result.Errors[0].ErrorMessage.ShouldBe(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
   }
 }
