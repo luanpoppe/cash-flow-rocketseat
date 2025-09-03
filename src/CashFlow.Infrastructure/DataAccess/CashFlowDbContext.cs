9e6984cec75f4d2c71f23f1b854e67206302d64a
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-03 18:56:05 -0300

    feat: criando classe DbContext

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,16 @@
+using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+  public DbSet<Expense> Expenses { get; set; }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=@Password123;";
+    var version = new MySqlServerVersion(new Version(8, 0, 43));
+    optionsBuilder.UseMySql(connectionString, version);
+  }
+}
