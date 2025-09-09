e033fd053da3e97de52890cf8a878b02c8ecf16c
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-09 12:10:45 -0300

    feat: utilizando o automapper de fato no use case e adicionando documentação da resposta para o swagger

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.API.Controllers;
@@ -9,6 +10,8 @@ namespace CashFlow.API.Controllers;
 public class ExpensesController : ControllerBase
 {
     [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterExpenseJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Register(
         [FromBody] RequestRegisterExpenseJson request,
         [FromServices] IRegisterExpenseUseCase useCase)
--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
@@ -12,30 +13,25 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
 {
   private readonly IExpensesRepository _repository;
   private readonly IUnityOfWork _unityOfWork;
+  private readonly IMapper _mapper;
 
-  public RegisterExpenseUseCase(IExpensesRepository repository, IUnityOfWork unityOfWork)
+  public RegisterExpenseUseCase(IExpensesRepository repository, IUnityOfWork unityOfWork, IMapper mapper)
   {
     _repository = repository;
     _unityOfWork = unityOfWork;
+    _mapper = mapper;
   }
 
   public async Task<ResponseRegisterExpenseJson> Execute(RequestRegisterExpenseJson request)
   {
     Validate(request);
 
-    var entity = new Expense
-    {
-      Amount = request.Amount,
-      Date = request.Date,
-      Description = request.Description,
-      Title = request.Title,
-      PaymentType = (PaymentType)request.PaymentType,
-    };
+    var entity = _mapper.Map<Expense>(request);
 
     await _repository.Add(entity);
     await _unityOfWork.Commit();
 
-    return new ResponseRegisterExpenseJson();
+    return _mapper.Map<ResponseRegisterExpenseJson>(entity);
   }
 
   private void Validate(RequestRegisterExpenseJson request)
