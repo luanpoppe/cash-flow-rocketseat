ccc7d2bd94de255550e76596d6aca7f9ce7302b7
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-09 12:35:46 -0300

    feat: adicionando rota de get all expenses

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,3 +1,4 @@
+using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
@@ -18,5 +19,17 @@ public class ExpensesController : ControllerBase
     {
         var response = await useCase.Execute(request);
         return Created(string.Empty, response);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseExpensesJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetAllExpenses([FromServices] IGetAllExpensesUseCase useCase)
+    {
+        var response = await useCase.Execute();
+
+        if (response.Expenses.Count != 0) return Ok(response);
+
+        return NoContent();
     }
 }
