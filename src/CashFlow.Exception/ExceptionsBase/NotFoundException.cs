39a956121376e2dde368df7309bb082005dbb4e6
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-10 11:38:03 -0300

    refactor: melhorando filtro de exceções para seguir melhor o princípio O do SOLID

--- src/CashFlow.Exception/ExceptionsBase/NotFoundException.cs
+++ src/CashFlow.Exception/ExceptionsBase/NotFoundException.cs
@@ -1,8 +1,17 @@
+using System.Net;
+
 namespace CashFlow.Exception.ExceptionsBase;
 
 public class NotFoundException : CashFlowException
 {
+  public override int StatusCode => (int)HttpStatusCode.NotFound;
+
   public NotFoundException(string message) : base(message)
   {
+  }
+
+  public override List<string> GetErrors()
+  {
+    return [Message];
   }
 }
