9189e67af16e325e1090f12ceeeb079f79555382
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-10 13:28:47 -0300

    feat: adicionando funcionalidade de deletar despesas

---
+++ src/CashFlow.Application/UseCases/Expenses/Delete/DeleteExpenseUseCase.cs
@@ -0,0 +1,29 @@
+
+using CashFlow.Domain.Repositories;
+using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Delete;
+
+public class DeleteExpenseUseCase : IDeleteExpenseUseCase
+{
+  private readonly IUnityOfWork _unityOfWork;
+  private readonly IExpensesWriteOnlyRepository _repository;
+  public DeleteExpenseUseCase(IExpensesWriteOnlyRepository repository, IUnityOfWork unityOfWork)
+  {
+    _repository = repository;
+    _unityOfWork = unityOfWork;
+  }
+  public async Task Execute(long id)
+  {
+    var result = await _repository.Delete(id);
+
+    if (result == false)
+    {
+      throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+    }
+
+    await _unityOfWork.Commit();
+  }
+}
