0d8f502e495fc068005a0308ff84b2bf8b6f80f3
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-13 16:54:57 -0300

    feat: adicionando funcionalidade de hash ao use case de registarr usuário

--- src/CashFlow.Application/AutoMapper/AutoMapping.cs
+++ src/CashFlow.Application/AutoMapper/AutoMapping.cs
@@ -16,7 +16,7 @@ public class AutoMapping : Profile
   private void RequestToEntity()
   {
     CreateMap<RequestExpenseJson, Expense>();
-    CreateMap<RequestRegisterUserJson, User>();
+    CreateMap<RequestRegisterUserJson, User>().ForMember(dest => dest.Password, config => config.Ignore());
   }
 
   private void EntityToResponse()
